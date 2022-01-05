6fb074d13c619699f493cdbba64d2f5667d818c1
Md Raihan <54474184+ryihan@users.noreply.github.com>
2022-01-05 17:39:40 +0600

    Update MainCameraController.cs

--- Assets/Script/MainCameraController.cs
+++ Assets/Script/MainCameraController.cs
@@ -6,14 +6,7 @@ public class MainCameraController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
-    {
-         
-      
-      
-       
-       
-
-    }
+    {}
     public void Scope (Transform  target,Vector3  railDirection){
         /*
         * object position Terget 
@@ -22,15 +15,12 @@ public class MainCameraController : MonoBehaviour
         Vector3 force = Vector3.Project(heading, railDirection);
         // GetComponent<Rigidbody>().AddForce(force);
        // Debug.Log(target.position);
-
     }
-
     // Update is called once per frame
     void Update()
     {
         Transform  target;
     float target = target.position.x;
         Debug.Log(target);
-       
     }
 }
