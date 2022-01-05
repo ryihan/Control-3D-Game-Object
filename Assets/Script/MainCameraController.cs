0c322540e8fa36a3aa5d982469e0adb184d98e09
raihan <54474184+ryihan@users.noreply.github.com>
2022-01-05 17:34:14 +0600

    Update MainCameraController.cs

--- Assets/Script/MainCameraController.cs
+++ Assets/Script/MainCameraController.cs
@@ -7,7 +7,9 @@ public class MainCameraController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+         
+      
+      
     }
 
     // Update is called once per frame
